4ebc5e90a3ed2f3ec3f6ed1e0f84e6cbacab4b0c
richard-berg-nti-johanneberg <60173062+richard-berg-nti-johanneberg@users.noreply.github.com>
2021-04-19 18:50:43 +0200

    hemma efter lek8

--- app.rb
+++ app.rb
@@ -7,40 +7,44 @@ require 'byebug'
 enable  :sessions
 
 get("/") do
-    slim(:homepage)
+  slim(:homepage)
 end
+
 
 get("/login") do
-    slim(:login)
+  slim(:login)
 end
+
 
 get("/register") do
-    slim(:register)
+  slim(:register)
 end
 
-get("/editprograms") do
-  id = session[:id].to_i
+
+post('/login') do
+  username = params[:username]
+  session[:username] = username
+  password = params[:password]
   db = SQLite3::Database.new('db/gym.db')
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM programs WHERE user_id = ?",id)
-  p result
-  ovningar_id = []
-  result.each do |program|
-    ovningar_id << db.execute("SELECT * FROM exercises_programs_relation WHERE programs_id = ?", program["id"])
-  end
-  p ovningar_id
-  ovningar = [] 
+  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  role = result["role"]
+  pwdigest = result["pwdigest"]
+  id = result["id"]
 
-  ovningar_id[0].each do |id| 
-    p id
-    p db.execute("SELECT content FROM exercises WHERE id = ?", id['exercises_id'])
-    # p db.execute("SELECT programs_id FROM exercises_programs_relation WHERE exercises_id = ?", id)
-    ovningar << [db.execute("SELECT content FROM exercises WHERE id = ?", id['exercises_id']), db.execute("SELECT programs_id FROM exercises_programs_relation WHERE exercises_id = ?", id['exercises_id'])]
-  end
-  p ovningar[0]
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    if role == 1
+      redirect('/programs/new')
+    else 
+      redirect("/programs")
+    end
 
-  slim(:editprograms,locals:{result:result,ovningar:ovningar})
+  else
+    "fel lösenord"
+  end
 end
+
 
 post('/users/new') do
   username = params[:username]
@@ -59,96 +63,91 @@ post('/users/new') do
   end
 end
 
+
 get("/programs") do
   id = session[:id].to_i
   db = SQLite3::Database.new('db/gym.db')
-  slim(:"programs", locals:{username:session[:username]})
-
+  result = db.execute("SELECT * FROM programs")
+  result2 = db.execute("SELECT * FROM programs_users_relation")
+  slim(:"/programs/index", locals:{username:session[:username],programname:result,addedprograms:result2})
 end
 
-get("/createprograms") do
+
+post("/programs") do
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/gym.db')
+
+  redirect("/programs")
+end
+
+
+get("/programs/new") do
   id = session[:id].to_i
   db = SQLite3::Database.new('db/gym.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM exercises WHERE user_id = ?",id)
   result2 = db.execute("SELECT * FROM programs WHERE user_id = ?",id)
 
-  slim(:"createprograms", locals:{exercises:result,username:session[:username],programname:result2})
+  slim(:"programs/new", locals:{exercises:result,username:session[:username],programname:result2})
 end
 
 
-post('/login') do
-    username = params[:username]
-    session[:username] = username
-    password = params[:password]
-    db = SQLite3::Database.new('db/gym.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
-    role = result["role"]
-    pwdigest = result["pwdigest"]
-    id = result["id"]
-  
-    if BCrypt::Password.new(pwdigest) == password
-      session[:id] = id
-      if role == 1
-        redirect('/createprograms')
-      else 
-        redirect("/programs")
-      end
-
-    else
-      "fel lösenord"
-    end
-  
+get("/programs/:id/edit") do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/gym.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT exercises.name FROM  exercises_programs_relation INNER JOIN exercises ON exercises_programs_relation.exercises_id = exercises.id WHERE programs_id = ?",id)
+  result2 = db.execute("SELECT name FROM programs WHERE id = ?", id)
+  slim(:"/programs/edit",locals:{exercises:result,program:result2})
 end
   
-
-post('/exercises/new') do
+post('/programs/exercises/new') do
   ovningsnamn = params[:ovningsnamn]
   userid = session[:id].to_i
   db = SQLite3::Database.new('db/gym.db')
   db.results_as_hash = true
-  db.execute("INSERT INTO exercises (content, user_id) VALUES (?,?)", ovningsnamn, userid)
+  db.execute("INSERT INTO exercises (name, user_id) VALUES (?,?)", ovningsnamn, userid)
 
-  redirect('/createprograms')
-
+  redirect('/programs/new')
 end
+
 
 post('/exercises/:id/delete') do
   id = params[:id].to_i
   db = SQLite3::Database.new('db/gym.db')
   db.execute("DELETE FROM exercises WHERE id = ?",id)
 
-  redirect('/createprograms')
-
+  redirect('/programs/new')
 end
 
-post('/programs/:id/delete') do
-  id = params[:id].to_i
-  db = SQLite3::Database.new('db/gym.db')
-  db.execute("DELETE FROM programs WHERE id = ?",id)
-  redirect('/createprograms')
 
-end
-
-post('/exercises/:id/update') do
+post('/exercises/:id/edit') do
   id = params[:id].to_i
-  content = params[:content]
+  name = params[:name]
   user_id = params[:user_id].to_i
   db = SQLite3::Database.new('db/gym.db')
-  db.execute("UPDATE exercises SET content=?, user_id=? WHERE id =?",content, user_id, id)
+  db.execute("UPDATE exercises SET name=?, user_id=? WHERE id =?",name, user_id, id)
 
-  redirect('/createprograms')
+  redirect('/programs/new')
 end
+
 
 get('/exercises/:id/edit') do
   id = params[:id].to_i 
   db = SQLite3::Database.new('db/gym.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM exercises WHERE id = ?",id).first
-  slim(:"/edit",locals:{result:result})
+  slim(:"/exercises/edit",locals:{result:result})
 end
 
+
+post('/programs/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/gym.db')
+  db.execute("DELETE FROM programs WHERE id = ?",id)
+
+  redirect('/programs/new')
+end
 
 
 post('/programs/new/') do
@@ -158,19 +157,17 @@ post('/programs/new/') do
   db = SQLite3::Database.new('db/gym.db')
   db.results_as_hash = true
   db.execute("INSERT INTO programs (name, user_id) VALUES (?,?)", name, userid)
-  
 
-  redirect('/createprograms')
+  redirect('/programs/new')
 end
 
+
 post('/programs/update/') do
-ovning = params[:ovning]
-program = params[:program]
-db = SQLite3::Database.new('db/gym.db')
-db.results_as_hash = true
-db.execute("INSERT INTO exercises_programs_relation (exercises_id, programs_id) VALUES (?,?)", ovning, program)
+  ovning = params[:ovning]
+  program = params[:program]
+  db = SQLite3::Database.new('db/gym.db')
+  db.results_as_hash = true
+  db.execute("INSERT INTO exercises_programs_relation (exercises_id, programs_id) VALUES (?,?)", ovning, program)
 
-
-redirect('/createprograms')
-
+  redirect('/programs/new')
 end
